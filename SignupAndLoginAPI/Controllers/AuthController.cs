a587e5ac52c155f6f5ddc3ac053f9ed3e83b96f1
Nagarjun-Test <smartnag341@gmail.com>
2025-09-30 19:36:54 -0500

    Extended Auth for google-login and google-callback. Added client id and credentials in appsettings.json

--- SignupAndLoginAPI/Controllers/AuthController.cs
+++ SignupAndLoginAPI/Controllers/AuthController.cs
@@ -1,9 +1,11 @@
-﻿using Google.Cloud.Firestore;
+﻿using Google.Apis.Auth;
+using Google.Cloud.Firestore;
 using Microsoft.AspNetCore.Mvc;
 using SignupAndLoginAPI.DTOs;
 using SignupAndLoginAPI.Models;
 using SignupAndLoginAPI.Services;
 using System.Text;
+using System.Text.Json;
 
 namespace SignupAndLoginAPI.Controllers
 {
@@ -17,6 +19,13 @@ namespace SignupAndLoginAPI.Controllers
         {
             _firestore = firestore;
         }
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
 
         [HttpPost("signup")]
         public async Task<IActionResult> Signup([FromBody] SignupDto dto)
@@ -81,6 +90,63 @@ namespace SignupAndLoginAPI.Controllers
 
             // For now just success, later return JWT
             return Ok(new { message = "Login successful", username = user.Username });
+        }
+
+
+        [HttpGet("google-login")]
+        public IActionResult GoogleLogin()
+        {
+            var redirectUrl = Url.Action("GoogleCallback", "Auth", null, Request.Scheme);
+            var clientId = _config["GoogleAuth:ClientId"];
+
+            var url = $"https://accounts.google.com/o/oauth2/v2/auth?response_type=code" +
+                      $"&client_id={clientId}" +
+                      $"&redirect_uri={redirectUrl}" +
+                      $"&scope=openid%20email%20profile";
+
+            return Redirect(url);
+        }
+
+        [HttpGet("google-callback")]
+        public async Task<IActionResult> GoogleCallback(string code)
+        {
+            var clientId = _config["GoogleAuth:ClientId"];
+            var clientSecret = _config["GoogleAuth:ClientSecret"];
+            var redirectUrl = Url.Action("GoogleCallback", "Auth", null, Request.Scheme);
+
+            using var http = new HttpClient();
+            var tokenResponse = await http.PostAsync("https://oauth2.googleapis.com/token",
+                new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+            {"code", code},
+            {"client_id", clientId},
+            {"client_secret", clientSecret},
+            {"redirect_uri", redirectUrl},
+            {"grant_type", "authorization_code"}
+                }));
+
+            var json = await tokenResponse.Content.ReadAsStringAsync();
+            var doc = JsonDocument.Parse(json);
+            var idToken = doc.RootElement.GetProperty("id_token").GetString();
+
+            var payload = await GoogleJsonWebSignature.ValidateAsync(idToken);
+
+            // Save or find user in Firestore
+            var user = await _firestore.GetUserByEmailAsync(payload.Email);
+            if (user == null)
+            {
+                user = new User
+                {
+                    Email = payload.Email,
+                    FirstName = payload.GivenName,
+                    LastName = payload.FamilyName,
+                    Username = payload.Email.Split('@')[0],
+                    PasswordHash = string.Empty
+                };
+                await _firestore.AddUserAsync(user);
+            }
+
+            return Ok(new { message = "Google Login successful", user.Email });
         }
     }
 }
